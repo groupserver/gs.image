4c2fb4c91030f517dd4d5cd838d92ecb9e162835
Michael JasonSmith <mpj17@onlinegroups.net>
2008-09-02 00:56:37 +0000

    Previous and next links, along with thumbnail support for the links.
    --HG--
    extra : convert_revision : svn%3A01e103ed-f575-20b1-d296-6440ccab0075/gs.image/trunk%404163

--- gs/image/queries.py
+++ gs/image/queries.py
@@ -21,3 +21,24 @@ class FileQuery(MessageQuery):
                   'topic':     self.topic(x['topic_id'])}
         return retval
 
+    def file_metadata_in_topic(self, topicId):
+        assert type(topicId) == str,\
+          'topicId is a %s not a string' % type(topicId)
+        ft = self.fileTable
+        
+        statement = ft.select()
+        statement.append_whereclause(ft.c.topic_id == topicId)
+        statement.order_by(ft.c.date)
+        
+        r = statement.execute()
+        retval = [{'file_id':   x['file_id'],
+                  'mime_type': x['mime_type'],
+                  'file_name': x['file_name'],
+                  'file_size': x['file_size'],
+                  'date':      x['date'],
+                  'post':      self.post(x['post_id'])} for x in r]
+
+        assert type(retval) == list
+        assert retval
+        return retval
+
--- gs/image/view.py
+++ gs/image/view.py
@@ -52,16 +52,29 @@ class GSImageView(BrowserView):
         
         self.__imageMetadata = None
         self.__authorInfo = None
+        self.__nextImage = None
+        self.__prevImage = None
+        self.__topicImages = None
         
         self.maxWidth = self.maxHeight = 432 # 24u on OGN
+
+        da = self.context.zsqlalchemy 
+        assert da, 'No data-adaptor found'
+        self.fileQuery = FileQuery(self.context, da)
 
     @property
     def scaledImageURI(self):
         # http://wibble.com/groups/bar/files/f/abc123/resize/500/500/foo.jpg
-        retval = '%s/files/f/%s/resize/%s/%s/%s' % \
-          (self.groupInfo.url, self.imageId, self.maxWidth, self.maxHeight, 
-           self.filename)
+        retval = self.get_uri_for_scaled(self.imageId, self.maxWidth, 
+                                         self.maxHeight, self.filename)
         assert self.imageId in retval
+        return retval
+    
+    def get_uri_for_scaled(self, imageId, maxWidth, maxHeight, filename):
+        retval = '%s/files/f/%s/resize/%s/%s/%s' % \
+          (self.groupInfo.url, imageId, maxWidth, maxHeight, filename)
+        assert type(retval) == str
+        assert retval
         return retval
     
     @property
@@ -70,7 +83,7 @@ class GSImageView(BrowserView):
         retval = '%s/files/f/%s/resize/%s' % \
           (self.groupInfo.url, self.imageId, self.filename)
         assert self.imageId in retval
-        return retval        
+        return retval
 
     @property
     def filename(self):
@@ -105,10 +118,45 @@ class GSImageView(BrowserView):
     @property
     def imageMetadata(self):
         if self.__imageMetadata == None:
-            da = self.context.zsqlalchemy 
-            assert da, 'No data-adaptor found'
-            fileQuery = FileQuery(self.context, da)
-            self.__imageMetadata = fileQuery.file_metadata(self.imageId)
+            iid = self.imageId
+            self.__imageMetadata = self.fileQuery.file_metadata(iid)
         retval = self.__imageMetadata
+        return retval
+
+    def images_in_topic(self):
+        if self.__topicImages == None:
+            topicId = self.imageMetadata['topic']['topic_id']
+            files = self.fileQuery.file_metadata_in_topic(topicId)
+            self.__topicImages = [f for f in files 
+                                  if 'image' in f['mime_type']]
+            for f in self.__topicImages:
+                iid = f['file_id']
+                fn = f['file_name']
+                f['icon_uri'] = self.get_uri_for_scaled(iid, 27, 27, fn)
+                print f['icon_uri']
+        retval = self.__topicImages
+        
+        assert type(retval) == list
+        assert retval
+        return retval
+    
+    @property
+    def prevImage(self):
+        if self.__prevImage == None:
+            files = self.images_in_topic()
+            currImgDate = self.imageMetadata['date']
+            prevs = [f for f in files if f['date'] < currImgDate]
+            self.__prevImage = prevs and prevs[-1] or None
+        retval = self.__prevImage
+        return retval
+
+    @property
+    def nextImage(self):
+        if self.__nextImage == None:
+            files = self.images_in_topic()
+            currImgDate = self.imageMetadata['date']
+            nexts = [f for f in files if f['date'] > currImgDate]
+            self.__nextImage = nexts and nexts[0] or None
+        retval = self.__nextImage
         return retval
 
