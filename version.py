ba8d426b50d04eca3770418aec347c28cefb6a5c
Richard Waid <richard@iopen.net>
2009-10-07 15:43:11 +1300

    Fix version to work with source distribution format

--- version.py
+++ version.py
@@ -2,15 +2,20 @@ version='1.0'
 release=False
 
 
-import commands
+import commands, os
 def get_version():
-    version_string = version
+    version_string = ''
     if not release:
         status,output = commands.getstatusoutput("hg tip --template '{node|short}'")
         if status != 0:
-            version_string += '-dev'
+            if os.path.exists('gs.image.egg-info/PKG-INFO'):
+                for line in file('gs.image.egg-info/PKG-INFO'):
+                    if line.find('Version: ') == 0:
+                        version_string = line.strip().split('Version: ')[1].strip()
+            if not version_string:
+                version_string = '%s-dev' % version
         else:
-            version_string += '-%s' % output
+            version_string += '%s-%s' % (version, output)
     
     return version_string
        
