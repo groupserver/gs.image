4d82c5d8a255036fda1ecd0303575949fa709e7a
Michael JasonSmith <mpj17@onlinegroups.net>
2011-02-15 14:15:19 +1100

    Moving the View Image page to gs.group.messages.image

--- gs/image/interfaces.py
+++ gs/image/interfaces.py
@@ -2,15 +2,6 @@ from zope.interface import Interface
 from zope.schema import Int
 from zope.app.file.interfaces import IImage
 
-class IGSImageView(Interface):
-    width = Int(title=u'Width',
-        description=u'The width of the image',
-        default=432)
-
-    height = Int(title=u'Height',
-        description=u'The height of the image',
-        default=432)
-
 class IGSImage(IImage):
     def get_resized(x, y, maintain_aspect=True): #@NoSelf
         u"""Resize an image
